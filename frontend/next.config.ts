112c550a28aba101b82c4a6c7a0051bbf79e40de
Beyza <beyza.capraz@metu.edu.tr>
2025-04-06 22:34:21 +0300

    vercel file change

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/details/:productId',
+        destination: '/details/[productId]',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
